26767f48c0b5fbd1bca5e1f6ef3348b72648a8be
ae12321 <80916217+ae12321@users.noreply.github.com>
2023-09-15 04:00:10 +0900

    typo

--- components/Post.tsx
+++ components/Post.tsx
@@ -5,7 +5,7 @@ export default function Post(props: { post: MyPost }) {
     <section className=" bg-lime-500 rounded-lg p-4 mb-4 hover:translate-y-1 transition-all duration-300">
       <div>
         <p className="text-xs">{props.post.createdAt}</p>
-        <Link href={`/post/${props.post.id}`}>
+        <Link href={`/posts/${props.post.id}`}>
           <p className="text-2xl">{props.post.title}</p>
         </Link>
         {props.post.tags.map((tag, index) => (
